1fd5cfe650dcedc89b0ee7d8901acb2b28fcbf47
sxjeru <sxjeru@gmail.com>
2022-08-17 10:17:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,13 @@ sudo apt install wget -y
 #unzip $Pack_Name.zip
 #cd $Open_Pack_Name
 #sudo chmod 777 gradlew
-sudo rm -rf /usr/lib/jvm/*
-sudo apt install openjdk-8-jdk
-unset JAVA_HOME
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin/java
-export JAVA_HOME
-sudo env
+#sudo rm -rf /usr/lib/jvm/*
+#sudo apt install openjdk-8-jdk
+#unset JAVA_HOME
+#JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin/java
+#export JAVA_HOME
+#sudo env
+sudo update-java-alternatives -l
 java -version
 sudo gradle -v
 #sudo ./gradlew build -Dfile.encoding=UTF-8 --info
