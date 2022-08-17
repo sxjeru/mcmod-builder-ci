65f57b4dc844b06e0c64ce1b98e2883ac994a2c5
sxjeru <sxjeru@gmail.com>
2022-08-17 08:43:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,17 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install wget -y
-wget $DL_URL
-unzip $Pack_Name.zip
-cd $Open_Pack_Name
+#wget $DL_URL
+#unzip $Pack_Name.zip
+#cd $Open_Pack_Name
 sudo chmod 777 gradlew
 sudo rm -rf /usr/lib/jvm/*
 sudo apt install openjdk-8-jdk
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin/java
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin/java
+export PATH=$JAVA_HOME:$PATH
 sudo env
 java -version
 sudo gradle -v
 #sudo ./gradlew build -Dfile.encoding=UTF-8 --info
-sudo ./gradlew build -Dorg.gradle.java.home='/usr/lib/jvm/java-8-openjdk-amd64/bin/java' -Dfile.encoding=UTF-8 --info
+#sudo ./gradlew build -Dorg.gradle.java.home='/usr/lib/jvm/java-8-openjdk-amd64/bin/java' -Dfile.encoding=UTF-8 --info
 cp -r build/libs /opt/libs
