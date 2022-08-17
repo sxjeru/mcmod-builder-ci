0a10f6dcd89a15d509bab1511ff650a0091c64d9
sxjeru <sxjeru@gmail.com>
2022-08-17 11:16:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,22 +2,22 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install wget -y
-#wget $DL_URL
-#unzip $Pack_Name.zip
-#cd $Open_Pack_Name
-#sudo chmod 777 gradlew
+wget $DL_URL
+unzip $Pack_Name.zip
+cd $Open_Pack_Name
+sudo chmod 777 gradlew
 #sudo rm -rf /usr/lib/jvm/*
 #sudo apt install openjdk-8-jdk
 sudo update-java-alternatives -s temurin-8-jdk-amd64
-unset JAVA_HOME
-sudo env
-JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
-export JAVA_HOME
-sudo env
+#unset JAVA_HOME
+#sudo env
+#JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
+#export JAVA_HOME
+#sudo env
 java -version
-sudo gradle -Dorg.gradle.java.home='/usr/lib/jvm/temurin-8-jdk-amd64' -v
-#sudo ./gradlew build -Dfile.encoding=UTF-8 --info
-#sudo ./gradlew build -Dorg.gradle.java.home='/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64' -Dfile.encoding=UTF-8 --info
+#sudo gradle -Dorg.gradle.java.home='/usr/lib/jvm/temurin-8-jdk-amd64' -v
+sudo ./gradlew -v
+sudo ./gradlew build -Dorg.gradle.java.home='/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64' -Dfile.encoding=UTF-8 --info
 cp -r build/libs /opt/libs
 
 
