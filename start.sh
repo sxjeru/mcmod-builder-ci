41458be521c924cf1279a2edcd29132895a7de8a
sxjeru <sxjeru@gmail.com>
2022-08-17 08:03:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install openjdk-8-jdk wget -y
-sudo find / -name java
+sudo apt install wget -y
 #wget $DL_URL
 #unzip $Pack_Name.zip
 #cd $Open_Pack_Name
 sudo chmod 777 gradlew
+set JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64/bin/java
 java -version
 #sudo ./gradlew build -Dfile.encoding=UTF-8 --info
 cp -r build/libs /opt/libs
